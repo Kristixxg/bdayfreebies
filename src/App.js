c47949dfa592db74df47b19f0e665751b35960d7
Kristy Guo <krisxxg@gmail.com>
2023-11-16 23:00:54 -0800

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Card from './components/Card';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Card />
+        <SearchBar />
 
       </header>
     </div>
---
+++ src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div class="search-container">
+      <form action="/search" method="get">
+        <input
+          class="search"
+          id="searchleft"
+          type="search"
+          name="search"
+          placeholder="Search a place..."
+        />
+        <label class="button searchbutton" for="searchleft">
+          <span class="mglass">&#9906;</span>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
